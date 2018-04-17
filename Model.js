bc7812f5f54e805da14d00f5a8fd4e2dea7af729
Iswanul Umam <umam@sepulsa.com>
2018-04-17 20:29:17 +0700

    finish setup db

---
+++ Model.js
@@ -0,0 +1,16 @@
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./address-book.db');
+
+class ContactsModel {
+  
+}
+
+class GroupsModes {
+
+}
+
+module.exports = {
+  ContactsModel,
+  GroupsModes
+}
