bf52692a88b7815104f905a24564fb0e16597549
A N Afifudin <33721254+davidjoshua87@users.noreply.github.com>
2018-04-18 11:22:13 +0700

    Edit mothod showContact

--- controllers/contact.js
+++ controllers/contact.js
@@ -10,23 +10,19 @@ const ContactModel = require('../models/contact');
 const View = require('../views/view');
 
 class ContactController {
+
    static addContact(name, company, phone_number, email) {
-      // ContactModel.phoneNumberValidasi(phone_number, (dis) => {
-      //    if (dis !== `phone_number is not valid`) {
       ContactModel.add(name, company, phone_number, email, (dis2) => {
          View.display(dis2);
       });
-      // }
-      // else {
-      //    View.display(dis);
-      // }
-      // });
    }
+
    static updateContact(name, company, phone_number, email, contact_id) {
       ContactModel.update(name, company, phone_number, email, contact_id, (dis) => {
          View.display(dis);
       });
    }
+   
    static deleteContact(contact_id) {
       ContactModel.delete(contact_id, (dis) => {
          View.display(dis);
@@ -35,7 +31,7 @@ class ContactController {
 
    static showContact(cb) {
       ContactModel.show((dis) => {
-         View.displayShow(dis);
+         View.displayShowContact(dis);
       });
    }
 }
