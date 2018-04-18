3014c3d5df1409bbcd9cd90ad5054276e95007bb
A N Afifudin <33721254+davidjoshua87@users.noreply.github.com>
2018-04-18 11:25:50 +0700

    Edit method add with added validasi phone number

--- models/contact.js
+++ models/contact.js
@@ -15,33 +15,31 @@ const db = new sqlite3.Database('./libs/address_book.db', (err) => {
 
 class ContactModel {
 
-   // static phoneNumberValidasi(phone_number, cb) {
-   //    let validNumber = '0123456789';
-   //    if (phone_number.length > 17) {
-   //       cb(`phone_number is not valid`);
-   //    }
-   //
-   //    for (let i = 0; i < phone_number.length; i++) {
-   //       let count = 0;
-   //       for (let j = 0; j < validNumber.length; j++) {
-   //          if (phone_number[i] == validNumber[j]) {
-   //             count++;
-   //          }
-   //       }
-   //       if (count == 0) {
-   //          cb(`phone_number is not valid`);
-   //       }
-   //    }
-   // }
-
    static add(name, company, phone_number, email, cb) {
-      db.run(`INSERT INTO Contacts (contact_id, name, company, phone_number, email) VALUES (
-           null, '${name}', '${company}', '${phone_number}', '${email}')`, (err) => {
-         if (err) throw err;
-         else {
-            cb('Table Contacts added successfully');
+      if (phone_number.length > 13) {
+         cb('phone_number is not valid');
+      } else {
+         let validNumber = '0123456789';
+         for (let i = 0; i < phone_number.length; i++) {
+            let count = 0;
+            for (let j = 0; j < validNumber.length; j++) {
+               if (phone_number[i] == validNumber[j]) {
+                  count++;
+               }
+            }
+            if (count == 0) {
+               cb(`phone_number is not valid`);
+            }
          }
-      });
+         db.run(`INSERT INTO Contacts (contact_id, name, company, phone_number, email) VALUES (
+             null, '${name}', '${company}', '${phone_number}', '${email}')`, (err) => {
+            if (err) throw err;
+            else {
+               cb('Table Contacts added successfully');
+            }
+         });
+      }
+
       db.close((err) => {
          if (err) {
             return console.error(err.message);
@@ -68,6 +66,7 @@ class ContactModel {
          console.log('Close the database connection.');
       });
    }
+   
    static delete(contact_id, cb) {
       db.run(`DELETE FROM Contacts WHERE contact_id = ${contact_id};`, (err) => {
          if (err) throw err;
@@ -84,10 +83,11 @@ class ContactModel {
    }
 
    static show(cb) {
-      db.all(`SELECT Contacts.name AS ContactName, company, phone_number, email, gabung.name AS groupName FROM Contacts
-            LEFT JOIN (SELECT groups.name, Contact_Groups.contact_id FROM groups
-            JOIN Contact_Groups ON groups.group_id = Contact_Groups.group_id) AS gabung
-            ON gabung.contact_id = Contacts.contact_id;`, (err, dataRows) => {
+      db.all(`SELECT Contacts.name AS contactName, company, phone_number, email, gabungGroup.name AS groupName FROM Contacts
+            LEFT JOIN (SELECT Groups.name, Contact_Groups.contact_id FROM Groups
+            JOIN Contact_Groups ON Groups.group_id = Contact_Groups.group_id) AS gabungGroup
+            ON gabungGroup.contact_id = Contacts.contact_id
+            ORDER BY gabungGroup.name ASC;`, (err, dataRows) => {
          if (err) throw err;
          else {
             cb(dataRows);
